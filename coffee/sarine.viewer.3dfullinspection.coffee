805dabe495acf8c5670d4d6338c831016a2b55d3
unknown <Adic@AdiC-pc.sarin.local>
2015-03-24 15:03:21 +0200

    change prefix

--- coffee/sarine.viewer.3dfullinspection.coffee
+++ coffee/sarine.viewer.3dfullinspection.coffee
@@ -1,10 +1,10 @@
 ###!
-sarine.viewer.3dfullinspection - v0.0.10 -  Tuesday, March 24th, 2015, 2:17:00 PM 
+sarine.viewer.3dfullinspection - v0.0.10 -  Tuesday, March 24th, 2015, 3:02:01 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 class FullInspection extends Viewer
   constructor: (options) ->
-    @resourcesPrefix = "//dev.sarineplatform.com/qa4/content/viewers/atomic/v1/assets/"
+    @resourcesPrefix = stones[0].viewersBaseUrl + "atomic/v1/assets/"
     @resources = [
       {element:'script',src:'jquery-ui.js'},
       {element:'script',src:'jquery.ui.ipad.altfix.js'},
--- dist/sarine.viewer.3dfullinspection.bundle.js
+++ dist/sarine.viewer.3dfullinspection.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.3dfullinspection - v0.0.10 -  Tuesday, March 24th, 2015, 2:17:00 PM 
+sarine.viewer.3dfullinspection - v0.0.10 -  Tuesday, March 24th, 2015, 3:02:01 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -80,7 +80,7 @@ sarine.viewer.3dfullinspection - v0.0.10 -  Tuesday, March 24th, 2015, 2:17:00 P
       this.first_init = __bind(this.first_init, this);
       this.convertElement = __bind(this.convertElement, this);
       this.preloadAssets = __bind(this.preloadAssets, this);
-      this.resourcesPrefix = "//dev.sarineplatform.com/qa4/content/viewers/atomic/v1/assets/";
+      this.resourcesPrefix = stones[0].viewersBaseUrl + "atomic/v1/assets/";
       this.resources = [
         {
           element: 'script',
