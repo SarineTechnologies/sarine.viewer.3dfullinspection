550fe4b992c7f0c4545c3e0a0b0acc93af35dcab
SarineColor <github@sarine.com>
2015-10-20 16:36:39 +0300

    deploy 0.33.0

--- dist/sarine.viewer.3dfullinspection.js
+++ dist/sarine.viewer.3dfullinspection.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.3dfullinspection - v0.32.0 -  Tuesday, October 20th, 2015, 4:00:26 PM 
+sarine.viewer.3dfullinspection - v0.33.0 -  Tuesday, October 20th, 2015, 4:36:36 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
