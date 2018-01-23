5e7cb588a389305d5c44c2f9c1c9ad88d3dd25bb
ronishoham <roni.shoham@sarine.com>
2018-01-23 12:05:49 +0200

    env feature

--- dist/sarine.viewer.3dfullinspection.bundle.js
+++ dist/sarine.viewer.3dfullinspection.bundle.js
@@ -1,75 +1,14 @@
 
 /*!
-sarine.viewer.3dfullinspection - v0.51.0 -  Tuesday, January 2nd, 2018, 4:56:48 PM 
+sarine.viewer.3dfullinspection - v0.51.0 -  Tuesday, January 23rd, 2018, 11:51:17 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
 (function() {
-  var FullInspection, Viewer, queryStringImpl,
+  var FullInspection, queryStringImpl,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
-  Viewer = (function() {
-    var error, rm;
-
-    rm = ResourceManager.getInstance();
-
-    function Viewer(options) {
-      console.log("");
-      this.first_init_defer = $.Deferred();
-      this.full_init_defer = $.Deferred();
-      this.src = options.src, this.element = options.element, this.autoPlay = options.autoPlay, this.callbackPic = options.callbackPic;
-      this.id = this.element[0].id;
-      this.element = this.convertElement();
-      Object.getOwnPropertyNames(Viewer.prototype).forEach(function(k) {
-        if (this[k].name === "Error") {
-          return console.error(this.id, k, "Must be implement", this);
-        }
-      }, this);
-      this.element.data("class", this);
-      this.element.on("play", function(e) {
-        return $(e.target).data("class").play.apply($(e.target).data("class"), [true]);
-      });
-      this.element.on("stop", function(e) {
-        return $(e.target).data("class").stop.apply($(e.target).data("class"), [true]);
-      });
-      this.element.on("cancel", function(e) {
-        return $(e.target).data("class").cancel().apply($(e.target).data("class"), [true]);
-      });
-    }
-
-    error = function() {
-      return console.error(this.id, "must be implement");
-    };
-
-    Viewer.prototype.first_init = Error;
-
-    Viewer.prototype.full_init = Error;
-
-    Viewer.prototype.play = Error;
-
-    Viewer.prototype.stop = Error;
-
-    Viewer.prototype.convertElement = Error;
-
-    Viewer.prototype.cancel = function() {
-      return rm.cancel(this);
-    };
-
-    Viewer.prototype.loadImage = function(src) {
-      return rm.loadImage.apply(this, [src]);
-    };
-
-    Viewer.prototype.setTimeout = function(delay, callback) {
-      return rm.setTimeout.apply(this, [this.delay, callback]);
-    };
-
-    return Viewer;
-
-  })();
-
-  this.Viewer = Viewer;
 
   FullInspection = (function(_super) {
     var Metadata, Preloader, STRIDE_X, UI, ViewerBI, config, isBucket, isLocal, magnifierLibName, qs, reqsPerHostAllowed;
